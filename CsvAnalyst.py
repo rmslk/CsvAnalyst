80bc68da30f2ce20cc6e31a4d36a5a906c340aa4
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-04 09:16:21 +0300

    Создание основы программы

---
+++ CsvAnalyst.py
@@ -0,0 +1,11 @@
+# Программа анализа .csv файлов
+
+import tkinter as tk
+
+# Создание главного окна
+window=tk.Tk()
+window.geometry("550x550")
+window.title("Программа анализа .csv файлов")
+
+# Запуск цикла mailloop
+window.mainloop()
