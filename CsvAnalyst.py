8bef56fd5f91d5e3fe726a31a67e0c52ec3eec4e
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-04 09:51:09 +0300

    Добавление диалога открытия файла

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText as st
 from tkinter import messagebox as mb
+from tkinter import filedialog as fd
 
 # Создание главного окна
 window=tk.Tk()
@@ -32,8 +33,14 @@ label_01.grid(row=0, column=1, sticky="w")
 output_text = st(heigh = 22, width = 50)
 output_text.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
+# Диалог открытия файла
+def do_dialog():
+    name= fd.askopenfilename()
+    return name
+
 # Обработчик нажатия кнопки
 def process_button():
+    do_dialog()
     mb.showinfo(title=None, message="Готово")
 
 # Создание кнопки
